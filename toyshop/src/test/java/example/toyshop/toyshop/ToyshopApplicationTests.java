fa030dc497de0d129240329f8bbc0995a71feabb
KimJunHyeong <jhkim4685@gmail.com>
2018-12-27 15:36:50 +0900

    init project toyshop

---
+++ toyshop/src/test/java/example/toyshop/toyshop/ToyshopApplicationTests.java
@@ -0,0 +1,17 @@
+package example.toyshop.toyshop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ToyshopApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
